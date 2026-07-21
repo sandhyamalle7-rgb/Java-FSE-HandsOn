37718dbe8b9b1a692e07f1e702a8411e78cde74f
M Sandhya <sandhyamalle7@gmail.com>
2026-07-21 15:39:42 +0530

    Added Week 2 Exercise 7 Constructor and Setter Injection

---
+++ WEEK 2/Exercise-7-Constructor-Setter-Injection/src/main/java/com/cognizant/injection/Main.java
@@ -0,0 +1,18 @@
+package com.cognizant.injection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Student student =
+                (Student) context.getBean("student");
+
+        student.display();
+    }
+}
