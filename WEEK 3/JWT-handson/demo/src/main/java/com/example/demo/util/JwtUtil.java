1ff15a87b5d20041580092d3891a3dcc1f33e092
Your Name <your-email@example.com>
2026-07-23 09:41:52 +0530

    Added Week 3 JWT handson project

---
+++ WEEK 3/JWT-handson/demo/src/main/java/com/example/demo/util/JwtUtil.java
@@ -0,0 +1,21 @@
+package com.example.demo.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
+public class JwtUtil {
+
+    private static final String SECRET_KEY = "cognizantjwtsecret";
+
+    public String generateToken(String username) {
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 600000))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+}
